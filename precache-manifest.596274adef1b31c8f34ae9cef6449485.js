0ef3d694d0b0f40aef939c816eaa1bbab30d885b
Manoj <manoj.gudivada1@gmail.com>
2020-08-16 16:10:32 +0530

    Updates

---
+++ precache-manifest.596274adef1b31c8f34ae9cef6449485.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b70381a2e91bb40808086dff1aa6a86",
+    "url": "/YouTube-hooks/index.html"
+  },
+  {
+    "revision": "66f0bd05d1c519fe2c0f",
+    "url": "/YouTube-hooks/static/css/2.2611e590.chunk.css"
+  },
+  {
+    "revision": "acd3d723978704de8bb4",
+    "url": "/YouTube-hooks/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "66f0bd05d1c519fe2c0f",
+    "url": "/YouTube-hooks/static/js/2.bc4c2f3d.chunk.js"
+  },
+  {
+    "revision": "4a1691fd44116c5f8c99a494e6966d73",
+    "url": "/YouTube-hooks/static/js/2.bc4c2f3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acd3d723978704de8bb4",
+    "url": "/YouTube-hooks/static/js/main.85681ee5.chunk.js"
+  },
+  {
+    "revision": "ab0a3cd40634aea678a5",
+    "url": "/YouTube-hooks/static/js/runtime-main.51e9ff72.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/YouTube-hooks/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/YouTube-hooks/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/YouTube-hooks/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/YouTube-hooks/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/YouTube-hooks/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/YouTube-hooks/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/YouTube-hooks/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/YouTube-hooks/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/YouTube-hooks/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/YouTube-hooks/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/YouTube-hooks/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/YouTube-hooks/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/YouTube-hooks/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/YouTube-hooks/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/YouTube-hooks/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/YouTube-hooks/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "adcaaacb587f79cbbd07bec2b25f992f",
+    "url": "/YouTube-hooks/static/media/youtubeLogo.adcaaacb.png"
+  }
+]);
